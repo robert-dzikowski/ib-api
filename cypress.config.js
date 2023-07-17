8d3d336fdd1bbbae803d8aa56e7e59f26192a376
Robert Dzikowski <rdzikowski@live.com>
2023-07-18 01:26:47 +0200

    Added test List users

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  responseTimeout: 10000,
   video: false,
+  screenshotOnRunFailure: false,
   chromeWebSecurity: false,
   reporter: 'junit',
   reporterOptions: {
-    mochaFile: 'results/test-results-[hash].xml',
+    mochaFile: 'results/test-results.xml',
   },
   e2e: {
     setupNodeEvents(on, config) {
